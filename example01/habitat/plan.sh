e12beafe84606d0dfb0af31323a7d041bf232f25
Jay Mundrawala <jay@thechamberofunderstanding.com>
2017-10-27 10:04:34 -0500

    Added a binding example
    This one starts a service that depends on postgresql optionally. It does
    not start postgresql, so the service never comes up.

---
+++ example01/habitat/plan.sh
@@ -0,0 +1,27 @@
+pkg_name=hab_binds_example01
+pkg_origin=jaym
+pkg_version=1.0.0
+pkg_maintainer="Jay Mundrawala <jay@thechamberofunderstanding.com>"
+pkg_license=('UNLICENSED')
+pkg_upstream_url="http://github.com/jaym/hab-binds"
+
+pkg_deps=(
+  core/coreutils
+  core/bash
+)
+
+pkg_binds_optional=(
+  [postgresql]="port superuser_name superuser_password"
+)
+pkg_description="Binds Example 1"
+pkg_upstream_url="https://www.chef.io/automate/"
+pkg_bin_dirs=(bin)
+pkg_lib_dirs=(lib)
+
+do_build() {
+  echo "build"
+}
+
+do_install() {
+  echo "install"
+}
